5e039a53f3d19d5f1951d2b83e7569d63a5dcfab
fasiondog <fasiondog@163.com>
2020-08-14 00:54:48 +0800

    try

---
+++ part/sg/ama.py
@@ -0,0 +1,17 @@
+from hikyuu import SG_Single
+
+name = "AMA"
+author = "fasiondog"
+doc = """AMA信号指示器
+使用《精明交易者》 [BOOK1]_ 中给出的曲线拐点算法判断曲线趋势，公式见下::
+
+    filter = percentage * STDEV((AMA-AMA[1], N)
+
+    Buy  When AMA - AMA[1] > filter
+    or Buy When AMA - AMA[2] > filter
+    or Buy When AMA - AMA[3] > filter 
+"""
+
+params = (('filter_n', 'int', 10), ('filter_p', 'float', 0.1))
+
+sg = SG_Single(AMA())
