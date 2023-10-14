e82ffa92b38733333627955e2be81e14adc6470e
fasiondog <fasiondog@163.com>
2023-10-14 15:00:21 +0800

    update

--- part/sg/ama/part.py
+++ part/sg/ama/part.py
@@ -1,4 +1,4 @@
-from hikyuu import SG_Single, AMA
+from hikyuu import SG_Single, AMA, CLOSE
 
 # 部件作者
 author = "fasiondog"
@@ -20,7 +20,7 @@ def part(filter_n=20, filter_p=0.1, ama_n=10, ama_fast_n=2, ama_slow_n=30):
     :param int filter_n: 
     :param float filter_p:
     """
-    return SG_Single(AMA(ama_n, ama_fast_n, ama_slow_n), filter_n=filter_n, filter_p=filter_p)
+    return SG_Single(AMA(CLOSE(), ama_n, ama_fast_n, ama_slow_n), filter_n=filter_n, filter_p=filter_p)
 
 
 if __name__ == '__main__':
