7abc5296a231b9d4f32876a2816bb3502704ca72
fasiondog <fasiondog@163.com>
2024-03-02 07:57:07 +0800

    add 累积收益曲线

---
+++ ind/累积收益曲线/part.py
@@ -0,0 +1,31 @@
+from hikyuu import *
+
+# 部件作者
+author = "fasiondog"
+
+# 版本
+version = '20231223'
+
+
+def part():
+    """
+    累积收益曲线
+
+    示例: 
+        ind = get_part('default.ind.累积收益曲线')
+        k = get_kdata('sz000001', Query(-200))
+        x = ind(k.close)
+        x.plot()
+    """
+    return ROCP(0) + 1
+
+
+if __name__ == '__main__':
+    from hikyuu.interactive import *
+    import matplotlib.pylab as plt
+    k = get_kdata("sz000001", Query(-300))
+    ind = part()
+    f = create_figure(2)
+    k.plot(axes=f[0])
+    ind(k.close).plot(axes=f[1])
+    plt.show()
