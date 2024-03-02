e5f6db77c56d74574e9d73a8549354cd152cfa58
fasiondog <fasiondog@163.com>
2024-03-02 08:08:34 +0800

    update

--- ind/累积收益率/part.py
+++ ind/累积收益率/part.py
@@ -9,10 +9,10 @@ version = '20231223'
 
 def part():
     """
-    累积收益曲线
+    累积收益率 (Cumulative return)
 
     示例: 
-        ind = get_part('default.ind.累积收益曲线')
+        ind = get_part('default.ind.累积收益率')
         k = get_kdata('sz000001', Query(-200))
         x = ind(k.close)
         x.plot()
