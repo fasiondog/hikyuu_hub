2b8071671aea9bd9527fb6ee8c409129d663d8d6
fasiondog <fasiondog@163.com>
2025-03-12 14:29:14 +0800

    update

--- sys/布林线挤压策略/part.py
+++ sys/布林线挤压策略/part.py
@@ -27,3 +27,50 @@ def part(boll_n: int = 20, band: float = 2.0, squeeze_n: int = 50):
     my_sys = SYS_Simple(sg=sg)
     my_sys.name = "布林线挤压策略"
     return my_sys
+
+
+if __name__ == "__main__":
+    # 执行 testall 命令时，会多传入一个参数，防止测试时间过长
+    # 比如如果在测试代码中执行了绘图操作，可以打开下面的注释代码
+    # 此时执行 testall 命令时，将直接返回
+    if len(sys.argv) > 1:
+        my_sys = part()
+        print(my_sys)
+        exit(0)
+
+    import os
+    import sys
+    if sys.platform == 'win32':
+        os.system('chcp 65001')
+
+    # 仅加载测试需要的数据，请根据需要修改
+    options = {
+        "stock_list": ["sz000001"],
+        "ktype_list": ["day"],
+        "load_history_finance": False,
+        "load_weight": False,
+        "start_spot": False,
+        "spot_worker_num": 1,
+    }
+    load_hikyuu(**options)
+
+    # 请在下方编写测试代码
+    local_hub = get_current_hub(__file__)
+    my_sys = part()
+    print(my_sys)
+
+    stk = sm[options['stock_list'][0]]
+    sg = my_sys.sg
+    sg.to = stk.get_kdata(Query(-200))
+    sg.plot()
+
+    my_tm = crtTM(Datetime(20150101), init_cash=0,
+                  cost_func=TC_FixedA2017())
+    my_sys.tm = my_tm
+    my_sys.mm = MM_FixedCount(100)
+    my_sys.mm.set_param("auto-checkin", True)
+    my_sys.run(stk, Query(Datetime(20150101)))
+
+    import matplotlib.pyplot as plt
+    my_sys.performance()
+    plt.show()
--- sys/趋势布林带/part.py
+++ sys/趋势布林带/part.py
@@ -25,3 +25,46 @@ def part(n: int = 100, band: float = 0.5, tm: TradeManager = None) -> System:
     my_tm = crtTM() if tm is None else tm
     my_sys = SYS_Simple(tm=my_tm, mm=MM_Nothing(), sg=my_sg)
     return my_sys.clone()
+
+
+if __name__ == "__main__":
+    # 执行 testall 命令时，会多传入一个参数，防止测试时间过长
+    # 比如如果在测试代码中执行了绘图操作，可以打开下面的注释代码
+    # 此时执行 testall 命令时，将直接返回
+    if len(sys.argv) > 1:
+        print("ignore test")
+        exit(0)
+
+    import os
+    import sys
+    if sys.platform == 'win32':
+        os.system('chcp 65001')
+
+    # 仅加载测试需要的数据，请根据需要修改
+    options = {
+        "stock_list": ["sz000001"],
+        "ktype_list": ["day"],
+        "load_history_finance": False,
+        "load_weight": False,
+        "start_spot": False,
+        "spot_worker_num": 1,
+    }
+    load_hikyuu(**options)
+
+    # 请在下方编写测试代码
+    local_hub = get_current_hub(__file__)
+    update_hub(local_hub)
+
+    my_sys = get_part(f"{local_hub}.sys.趋势布林带")
+    print(my_sys)
+
+    if len(sys.argv) <= 1:
+        my_tm = crtTM(Datetime(20200101), init_cash=100000,
+                      cost_func=TC_FixedA2017())
+        my_sys.tm = my_tm
+        my_sys.run(sm['sz000001'], Query(Datetime(20200101)))
+        print(my_sys.tm)
+        my_sys.tm.tocsv(os.path.dirname(os.path.abspath(__file__)))
+        my_sys.performance()
+        import matplotlib.pylab as plt
+        plt.show()
